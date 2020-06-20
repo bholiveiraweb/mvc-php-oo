841c648b62f04b937574f94448ce439ba55826a8
BRUNO H F OLIVEIRA <bholiveira.web@gmail.com>
2020-06-20 01:51:20 -0300

    Update Routes

--- App/Routes.php
+++ App/Routes.php
@@ -9,7 +9,7 @@ Router::base(BASE_URL);
  *  Public Routes
  *  ---------------------------------------------
  */
-Router::namespace('App\\Resources\\');
+Router::namespace('App\Resources');
 
 Router::get('/', 'Welcome::index');
 Router::get('/dashboard', 'Welcome::dashboard');
@@ -22,7 +22,7 @@ Router::get('/product', 'Product::index');
  *  404 Error Handler
  * ----------------------------------------------
  */
-Router::namespace('App\\Resources\\Errors\\');
+Router::namespace('App\Resources\Errors');
 Router::get('/error/404', 'Error404::index');
 
 if (Router::error()['404']) {
